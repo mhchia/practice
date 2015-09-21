512487d6460bad5e03323019521b75c7cccdd0f4
MaiHsuanChia <j84255801912@gmail.com>
2015-09-21 23:01:35 +0800

    image

--- ctf/hw0/image/test.py
+++ ctf/hw0/image/test.py
@@ -1,3 +1,4 @@
+import binascii
 import sys
 
 from PIL import Image
@@ -5,13 +6,32 @@ from PIL import Image
 im = Image.open("stego-e27e53959c0cf1dc83e52946ecc4044a.png")
 pix = im.load()
 
+new_image = Image.new("RGB",im.size)
+newim = new_image.load()
+
+
+
 string = ""
 for i in xrange(im.size[0]):
     for j in xrange(im.size[1]):
-        a = pix[i, j][2] & 0x01
+        a = bytes(pix[i, j][2])[0] & 1
+        '''
         if a == 1:
             sys.stdout.write('.')
         else:
             sys.stdout.write(' ')
+        '''
         string += str(a)
-
+        '''
+        if pix[i, j][2] & 1:
+            newim[i, j] = (0, 0, 0)
+        else:
+            newim[i, j] = (255, 255, 255)
+        '''
+'''
+string = "0b" + string[::-1]
+n = int(string, 2)
+print binascii.unhexlify('%x' % n)
+'''
+print string
+new_image.save('newnew.png')
